e4bd724c782ab009fccf95371f0d72844da8c3b7
Garrett Green <garrett.wile.green@gmail.com>
2020-12-23 16:53:08 -0500

    added from pp with noah

--- tests/server/index.spec.ts
+++ tests/server/index.spec.ts
@@ -2,10 +2,10 @@ import request from 'supertest';
 import app from '../../src/server';
 import { mockLinkModel } from '../mocks';
 
-const server = app.listen(3001);
+// const server = app.listen(3001);
 
 describe('Test Express Server', () => {
-  it('should POST /api/', async (done) => {
+  it('should POST /api/', async () => {
     const res = await request(app)
       .post('/api/')
       .send({ URL: mockLinkModel.URL });
@@ -14,11 +14,21 @@ describe('Test Express Server', () => {
     expect(res.body.URL).toBe(mockLinkModel.URL);
     expect(res.body).toHaveProperty('_id');
     expect(res.body).toHaveProperty('shortURL');
+  });
 
-    await new Promise((resolve) => {
-      return setTimeout(resolve, 5);
-    });
+  it('should GET /:shortURL', async () => {
+    const res = await request(app).get(`/${mockLinkModel.shortURL}`);
+    // console.log('it -> res', res);
 
-    return done();
+    expect(res.status).toBe(302);
+    expect(res.redirect).toBe(true);
+    expect(res.headers.location).toBe(mockLinkModel.URL);
+  });
+
+  it('should call next() on unknown shortURL', async () => {
+    const res = await request(app).get('/no-link');
+
+    expect(res.status).toBe(200);
+    expect(res.redirect).toBe(false);
   });
 });
